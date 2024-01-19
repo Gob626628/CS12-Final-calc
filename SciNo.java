22bfb7034d4597273a1e850b318de85a52e98ad9
Gob626628 <150839196+Gob626628@users.noreply.github.com>
2024-01-19 10:19:13 -0600

    Add files via upload
    scientific notation

---
+++ SciNo.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class SciNo {
+
+    public static void main( String[] args){
+// this is a test
+
+        long te1= 120000000;
+        System.out.println(te1.length());// it dont work but the plan is to take the length of the # then -1 boom
+
+
+        if(te1>0){// this is MAKING a SciNo
+            System.out.println("it works but no numbers");
+        long an1= te1/(10^te1.len()-1);
+        System.out.println(an1);
+
+        }
+        else{
+            System.out.println("test");
+        }
+
+
+
+
+
+    }
+}
